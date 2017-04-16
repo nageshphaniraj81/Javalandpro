30ed54d53431dc7158e889b280eb17aec1708196
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-04-16 12:28:46 +0530

    Tests DataTypes

---
+++ JavalandPro1.0/JavaWrittenTest/DataTypes/DataTypes.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		// 'A' starts from 65
+		// 'a' starts from 97
+		
+		char z = 'A'+55;
+		System.out.println(z);
+		
+	}
+
+}
